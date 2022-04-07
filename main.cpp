d69102a18dfb05b93e98f5c134c30af5732a6476
494xad <494xad@mail.ru>
2022-04-07 23:17:38 +0700

    Реализация методов класса стека
    Реализован конструктор и метод добавления элементов в стек

--- main.cpp
+++ main.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
 
-int main(int argc, char *argv[])
+template<typename T>
+class CustomStack
 {
-    std::cout << "Test";
-    return 0;
+public:
+
+    CustomStack() {
+        itemsCount = 0;
+        lastItemPtr = nullptr;
+    }
+
+    ~CustomStack() {
+
+    }
+
+    // Добавление нового элемента
+    void push(const T item) {
+        // последний элемент в стеке становится предпоследним
+        lastItemPtr = new Item{ lastItemPtr, item };
+        itemsCount++;
+    }
+
+    // Извлечение элемента
+    T pop() {
+
+    }
+
+    // Получение количества элементов в стеке
+    size_t getSize() {
+        return itemsCount;
+    }
+
+private:
+
+    // Переменная хранящая количество элементов
+    size_t		itemsCount;
+
+    // Структрура, описывающая элементы в стеке, стек выполнен на основе связного списка
+    struct Item {
+
+        // Указатель на предыдущий элемент в стеке
+        Item* prevItem;
+
+        // Данные в элементе стека
+        T data;
+
+    };
+
+    // Указатель на вршину стека (первый с конца)
+    Item*		lastItemPtr;
+};
+
+int main()
+{
+    CustomStack<int> myStack;
+    myStack.push(1);
+    myStack.push(2);
+
+    std::cout << myStack.getSize();
 }
